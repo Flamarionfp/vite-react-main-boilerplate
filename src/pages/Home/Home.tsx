586bb6a181ae839b3b10629688b900d5ce9a4aad
flamarionfagundes <flamarion.fagundes@ighthouseit.com.br>
2022-09-24 22:25:07 -0300

    make the home page layout responsive

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,22 @@
 import { Header, TechnologiesShowcase } from 'components';
+import useMediaQuery from 'beautiful-react-hooks/useMediaQuery';
 
 export const Home = () => {
+  const isHeaderVisible = useMediaQuery('(min-width: 731px)');
+
   return (
     <div className="bg-gradient-to-r from-sky-500 to-indigo-500 min-h-screen">
-      <Header />
-      <main className="flex flex-col items-center">
+      {isHeaderVisible && <Header />}
+      <main className="px-3 flex flex-col items-center">
         <header className="mt-40 mb-16">
-          <h2 className="text-white text-5xl font-extrabold">
+          {!isHeaderVisible && (
+            <div>
+              <h1 className="text-white text-center font-extrabold text-4xl mb-10">
+                React Vite Boilerplate
+              </h1>
+            </div>
+          )}
+          <h2 className="text-white text-center font-extrabold text-3xl lg:text-5xl">
             The best boilerplate for you React Project!
           </h2>
         </header>
@@ -15,7 +25,7 @@ export const Home = () => {
         </div>
       </main>
       <footer>
-        <p className="font-light text-white text-center text-sm mt-10">
+        <p className="font-light text-white text-center text-md mt-10">
           By Flamarion Fagundes - 2022 ©
         </p>
       </footer>
