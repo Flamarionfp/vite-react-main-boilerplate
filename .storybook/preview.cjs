315f375d3e4323b3a26eec12289bd2ed14c5e91f
flamarionfagundes <flamarion.fagundes@ighthouseit.com.br>
2022-10-09 21:39:47 -0300

    chore: set dark theme to storybook

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,3 +1,4 @@
+import { themes } from '@storybook/theming';
 import '../src/styles/global.css';
 
 export const parameters = {
@@ -7,5 +8,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  docs: {
+    theme: themes.dark,
   },
 }
